5fc8dd12e7ad00d07436a247d1ef90cccd7be17b
BeautifulHao <78714011@qq.com>
2020-11-30 18:31:31 +0800

    bottom nav

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -1,35 +1,41 @@
 import React from "react";
 import { renderRoutes } from "react-router-config";
-import { NavLink } from "react-router-dom";
 import styles from './home.module.less'
+import cls from 'classnames';
+import SvgIcon from '../components/icon-svg/index'
 
 function HomeLayout(props: any) {
-  const { route } = props;
+  const { route, location: { pathname } } = props;
+  const itemCls = (name: string) => cls({
+    [styles.item]: true,
+    [styles.active]: pathname === name,
+  })
+  const handleClick = (path: string) => {
+    if (path === pathname) return
+    props.history.push(path)
+  }
 
   return (
     <div className={styles.root}>
       {renderRoutes(route.routes)}
       <div className={styles["bottom-nav"]}>
-        <NavLink to="/home" activeClassName={styles["selected"]}>
-          <div className={styles["nav-item"]}>
-            <span>主页</span>
-          </div>
-        </NavLink>
-        <NavLink to="/about" activeClassName={styles["selected"]}>
-          <div className={styles["nav-item"]}>
-            <span>关于</span>
-          </div>
-        </NavLink>
-        <NavLink to="/compass" activeClassName={styles["selected"]}>
-          <div className={styles["nav-item"]}>
-            <span>发现</span>
-          </div>
-        </NavLink>
-        <NavLink to="/profile" activeClassName={styles["selected"]}>
-          <div className={styles["nav-item"]}>
-            <span>我的</span>
-          </div>
-        </NavLink>
+        <div className={itemCls('/home')} onClick={() => handleClick('/home')}>
+          <SvgIcon className={cls(styles.icon, styles.scale)} name="#elem" />
+          <h1 className={styles.text}>主页</h1>
+        </div>
+
+        <div className={itemCls('/compass')} onClick={() => handleClick('/compass')}>
+          <SvgIcon className={styles.icon} name="#compass" />
+          <h1 className={styles.text}>发现</h1>
+        </div>
+        <div className={itemCls('/about')} onClick={() => handleClick('/about')}>
+          <SvgIcon className={styles.icon} name="#form" />
+          <h1 className={styles.text}>关于</h1>
+        </div>
+        <div className={itemCls('/profile')} onClick={() => handleClick('/profile')}>
+          <SvgIcon className={styles.icon} name="#people" />
+          <h1 className={styles.text}>我的</h1>
+        </div>
       </div>
     </div>
   );
