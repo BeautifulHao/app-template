fd429be8396689edb99d33cccf806661476059c2
BeautifulHao <78714011@qq.com>
2020-12-02 18:40:52 +0800

    img

---
+++ src/components/title-bar/index.tsx
@@ -0,0 +1,14 @@
+
+
+import React, { FC } from 'react'
+import styles from './index.module.less'
+
+const TitleBar: FC<any> = ({ title }) => (
+  <div className={styles.title}>
+    <div className={styles.split} />
+    <h1 className={styles.text}>{title}</h1>
+    <div className={styles.split} />
+  </div>
+)
+
+export default TitleBar
