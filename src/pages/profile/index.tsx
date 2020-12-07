17055a13df3f568580c7bb84f2d7bd8eb144409d
BeautifulHao <78714011@qq.com>
2020-12-07 16:33:18 +0800

    profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,18 +1,88 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from "react-redux";
 import * as actionCreators from "../../stores/user/actionCreators";
 import { withRouter } from "react-router-dom";
+import styles from './index.module.less'
+import SvgIcon from '../../components/icon-svg/index';
+import avatarUrl from '../../assets/img/touxiang.webp';
 
 const Profile = (props: any) => {
     const { userInfo = {}, LoginByUserDispatch, loginStatus } = props;
-    const getUser = () => {
+
+    useEffect(() => {
         LoginByUserDispatch("hello", "world")
-    }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
-        <div>
-            <div>{userInfo.cname}</div>
-            <div>Login:{loginStatus?"True":"False"}</div>
-            <button onClick={getUser}>Login</button>
+        <div className={styles.root}>
+            <div className={styles.profile}>
+                <div className={styles.avatar}>
+                    {
+                        loginStatus ? (
+                            <img src={avatarUrl} className={styles.img} alt="" />
+                        ) : <SvgIcon name="#avatar" className={styles.icon} />
+                    }
+                </div>
+                <div className={styles.desc}>
+                    <p className={styles.info}>
+                        {
+                            !loginStatus ? '登陆/注册' : userInfo.name
+                        }
+                    </p>
+                    <p className={styles.text}>
+                        <SvgIcon name="#iphone" className={styles.icon} />
+                        <span>
+                            {
+                                !loginStatus ? '登陆后享受更多特权' : userInfo.ccity
+                            }
+                        </span>
+                    </p>
+                </div>
+                <SvgIcon name="#right" className={styles.iconRight} />
+            </div>
+
+            <div className={styles.colums}>
+                <div className={styles.item}>
+                    <div className={styles.icon}>
+                        <SvgIcon name="#gold" className={styles.icon} />
+                    </div>
+                    <p className={styles.desc}>钱包</p>
+                </div>
+                <div className={styles.item}>
+                    <div className={styles.icon}>
+                        <SvgIcon name="#purse" className={styles.icon} />
+                    </div>
+                    <p className={styles.desc}>福袋</p>
+                </div>
+                <div className={styles.item}>
+                    <div className={styles.icon}>
+                        <SvgIcon name="#red-packet" className={styles.icon} />
+                    </div>
+                    <p className={styles.desc}>红包</p>
+                </div>
+            </div>
+
+            <div className={styles.list}>
+                <div className={styles.item} onClick={() => alert('/order')}>
+                    <div className={styles.icon}>
+                        <SvgIcon name="#drumstick" />
+                    </div>
+                    <p className={styles.desc}>我的订单</p>
+                    <SvgIcon name="#right" className={styles['icon-right']} />
+                </div>
+            </div>
+
+            <div className={styles.list}>
+                <div className={styles.item} onClick={() => alert('/address')}>
+                    <div className={styles.icon}>
+                        <SvgIcon name="#carrot" />
+                    </div>
+                    <p className={styles.desc}>我的地址</p>
+                    <SvgIcon name="#right" className={styles['icon-right']} />
+                </div>
+            </div>
+
         </div>
     );
 }
