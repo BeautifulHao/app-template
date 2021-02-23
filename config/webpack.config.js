f5f3f9d42517a4c53d50e0a810f5ca2efe796d06
傅浩 <fuhao@fuhaodeMacBook-Pro.local>
2021-02-23 14:31:45 +0800

    postcss for antd-mobile

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -103,11 +103,9 @@ module.exports = function (webpackEnv) {
         loader: MiniCssExtractPlugin.loader,
         // css is located in `static/css`, use '../../' to locate index.html folder
         // in production `paths.publicUrlOrPath` can be a relative path
-        options: paths.publicUrlOrPath.startsWith('.') ?
-          {
-            publicPath: '../../'
-          } :
-          {},
+        options: paths.publicUrlOrPath.startsWith('.') ? {
+          publicPath: '../../'
+        } : {},
       },
       {
         loader: require.resolve('css-loader'),
@@ -122,49 +120,48 @@ module.exports = function (webpackEnv) {
           // Necessary for external CSS imports to work
           // https://github.com/facebook/create-react-app/issues/2677
           ident: 'postcss',
-          plugins: () => [
-            require('postcss-px-to-viewport')({
-              // 需要转换的单位，默认为"px"
-              unitToConvert: 'px',
-              // 设计稿的视口宽度, 将根据视口做比例换算
-              viewportWidth: 750,
-              // 转化精度，转换后保留位数
-              unitPrecision: 5,
-              // 能转化为vw的属性列表
-              propList: ['*'],
-              // 希望使用的视口单位
-              viewportUnit: 'vw',
-              // 字体使用的视口单位
-              fontViewportUnit: 'vw',
-              // 需要忽略的CSS选择器
-              selectorBlackList: [],
-              // 最小的转换数值
-              minPixelValue: 1,
-              // 媒体查询里的单位是否需要转换单位
-              mediaQuery: false,
-              // 转换后是否添加备用单位
-              replace: true,
-              // 忽略文件目录
-              exclude: [/node_modules/],
-              // 是否添加根据 landscapeWidth 生成的媒体查询条件 @media (orientation: landscape)
-              landscape: false,
-              // 横屏时使用的单位
-              landscapeUnit: 'vh',
-              // 横屏时使用的视口宽度
-              landscapeWidth: 1334
-            }),
-            require('postcss-flexbugs-fixes'),
-            require('postcss-preset-env')({
-              autoprefixer: {
-                flexbox: 'no-2009',
-              },
-              stage: 3,
-            }),
-            // Adds PostCSS Normalize as the reset css with default options,
-            // so that it honors browserslist config in package.json
-            // which in turn let's users customize the target behavior as per their needs.
-            postcssNormalize(),
-          ],
+          plugins: (loaderContext) => {
+            const designWidth = loaderContext.resourcePath.includes(path.join('node_modules', 'antd-mobile')) ? 375 : 750;
+            return [
+              require('postcss-px-to-viewport')({
+                // 需要转换的单位，默认为"px"
+                unitToConvert: 'px',
+                // 设计稿的视口宽度, 将根据视口做比例换算
+                viewportWidth: designWidth,
+                // 转化精度，转换后保留位数
+                unitPrecision: 5,
+                // 能转化为vw的属性列表
+                propList: ['*'],
+                // 希望使用的视口单位
+                viewportUnit: 'vw',
+                // 字体使用的视口单位
+                fontViewportUnit: 'vw',
+                // 需要忽略的CSS选择器
+                selectorBlackList: [],
+                // 最小的转换数值
+                minPixelValue: 1,
+                // 媒体查询里的单位是否需要转换单位
+                mediaQuery: false,
+                // 转换后是否添加备用单位
+                replace: true,
+                // 忽略文件目录
+                //exclude: [/node_modules/],
+                // 是否添加根据 landscapeWidth 生成的媒体查询条件 @media (orientation: landscape)
+                landscape: false
+              }),
+              require('postcss-flexbugs-fixes'),
+              require('postcss-preset-env')({
+                autoprefixer: {
+                  flexbox: 'no-2009',
+                },
+                stage: 3,
+              }),
+              // Adds PostCSS Normalize as the reset css with default options,
+              // so that it honors browserslist config in package.json
+              // which in turn let's users customize the target behavior as per their needs.
+              postcssNormalize(),
+            ]
+          },
           sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment,
         },
       },
@@ -182,7 +179,7 @@ module.exports = function (webpackEnv) {
           sourceMap: true,
           lessOptions: {
             javascriptEnabled: true
-         }
+          }
         },
       });
     }
@@ -196,34 +193,31 @@ module.exports = function (webpackEnv) {
     devtool: isEnvProduction ?
       shouldUseSourceMap ?
       'source-map' :
-      false :
-      isEnvDevelopment && 'cheap-module-source-map',
+      false : isEnvDevelopment && 'cheap-module-source-map',
     // These are the "entry points" to our application.
     // This means they will be the "root" imports that are included in JS bundle.
-    entry: isEnvDevelopment && !shouldUseReactRefresh ?
-      [
-        // Include an alternative client for WebpackDevServer. A client's job is to
-        // connect to WebpackDevServer by a socket and get notified about changes.
-        // When you save a file, the client will either apply hot updates (in case
-        // of CSS changes), or refresh the page (in case of JS changes). When you
-        // make a syntax error, this client will display a syntax error overlay.
-        // Note: instead of the default WebpackDevServer client, we use a custom one
-        // to bring better experience for Create React App users. You can replace
-        // the line below with these two lines if you prefer the stock client:
-        //
-        // require.resolve('webpack-dev-server/client') + '?/',
-        // require.resolve('webpack/hot/dev-server'),
-        //
-        // When using the experimental react-refresh integration,
-        // the webpack plugin takes care of injecting the dev client for us.
-        webpackDevClientEntry,
-        // Finally, this is your app's code:
-        paths.appIndexJs,
-        // We include the app code last so that if there is a runtime error during
-        // initialization, it doesn't blow up the WebpackDevServer client, and
-        // changing JS code would still trigger a refresh.
-      ] :
+    entry: isEnvDevelopment && !shouldUseReactRefresh ? [
+      // Include an alternative client for WebpackDevServer. A client's job is to
+      // connect to WebpackDevServer by a socket and get notified about changes.
+      // When you save a file, the client will either apply hot updates (in case
+      // of CSS changes), or refresh the page (in case of JS changes). When you
+      // make a syntax error, this client will display a syntax error overlay.
+      // Note: instead of the default WebpackDevServer client, we use a custom one
+      // to bring better experience for Create React App users. You can replace
+      // the line below with these two lines if you prefer the stock client:
+      //
+      // require.resolve('webpack-dev-server/client') + '?/',
+      // require.resolve('webpack/hot/dev-server'),
+      //
+      // When using the experimental react-refresh integration,
+      // the webpack plugin takes care of injecting the dev client for us.
+      webpackDevClientEntry,
+      // Finally, this is your app's code:
       paths.appIndexJs,
+      // We include the app code last so that if there is a runtime error during
+      // initialization, it doesn't blow up the WebpackDevServer client, and
+      // changing JS code would still trigger a refresh.
+    ] : paths.appIndexJs,
     output: {
       // The build folder.
       path: isEnvProduction ? paths.appBuild : undefined,
@@ -232,14 +226,12 @@ module.exports = function (webpackEnv) {
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
       filename: isEnvProduction ?
-        'static/js/[name].[contenthash:8].js' :
-        isEnvDevelopment && 'static/js/bundle.js',
+        'static/js/[name].[contenthash:8].js' : isEnvDevelopment && 'static/js/bundle.js',
       // TODO: remove this when upgrading to webpack 5
       futureEmitAssets: true,
       // There are also additional JS chunk files if you use code splitting.
       chunkFilename: isEnvProduction ?
-        'static/js/[name].[contenthash:8].chunk.js' :
-        isEnvDevelopment && 'static/js/[name].chunk.js',
+        'static/js/[name].[contenthash:8].chunk.js' : isEnvDevelopment && 'static/js/[name].chunk.js',
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
@@ -249,8 +241,7 @@ module.exports = function (webpackEnv) {
         info =>
         path
         .relative(paths.appSrc, info.absoluteResourcePath)
-        .replace(/\\/g, '/') :
-        isEnvDevelopment &&
+        .replace(/\\/g, '/') : isEnvDevelopment &&
         (info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')),
       // Prevents conflicts when multiple webpack runtimes (from different apps)
       // are used on the same page.
@@ -307,16 +298,14 @@ module.exports = function (webpackEnv) {
         new OptimizeCSSAssetsPlugin({
           cssProcessorOptions: {
             parser: safePostCssParser,
-            map: shouldUseSourceMap ?
-              {
-                // `inline: false` forces the sourcemap to be output into a
-                // separate file
-                inline: false,
-                // `annotation: true` appends the sourceMappingURL to the end of
-                // the css file, helping the browser find the sourcemap
-                annotation: true,
-              } :
-              false,
+            map: shouldUseSourceMap ? {
+              // `inline: false` forces the sourcemap to be output into a
+              // separate file
+              inline: false,
+              // `annotation: true` appends the sourceMappingURL to the end of
+              // the css file, helping the browser find the sourcemap
+              annotation: true,
+            } : false,
           },
           cssProcessorPluginOptions: {
             preset: ['default', {
@@ -420,7 +409,7 @@ module.exports = function (webpackEnv) {
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
             {
-              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/,/\.webp$/],
+              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.webp$/],
               loader: require.resolve('url-loader'),
               options: {
                 limit: imageInlineSizeLimit,
@@ -459,7 +448,10 @@ module.exports = function (webpackEnv) {
                   isEnvDevelopment &&
                   shouldUseReactRefresh &&
                   require.resolve('react-refresh/babel'),
-                  ["import", { libraryName: "antd-mobile", style: 'css' }]
+                  ["import", {
+                    libraryName: "antd-mobile",
+                    style: 'css'
+                  }]
                 ].filter(Boolean),
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
@@ -512,8 +504,7 @@ module.exports = function (webpackEnv) {
               use: getStyleLoaders({
                 importLoaders: 1,
                 sourceMap: isEnvProduction ?
-                  shouldUseSourceMap :
-                  isEnvDevelopment,
+                  shouldUseSourceMap : isEnvDevelopment,
               }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
@@ -528,8 +519,7 @@ module.exports = function (webpackEnv) {
               use: getStyleLoaders({
                 importLoaders: 1,
                 sourceMap: isEnvProduction ?
-                  shouldUseSourceMap :
-                  isEnvDevelopment,
+                  shouldUseSourceMap : isEnvDevelopment,
                 modules: {
                   getLocalIdent: getCSSModuleLocalIdent,
                 },
@@ -544,8 +534,7 @@ module.exports = function (webpackEnv) {
               use: getStyleLoaders({
                   importLoaders: 3,
                   sourceMap: isEnvProduction ?
-                    shouldUseSourceMap :
-                    isEnvDevelopment,
+                    shouldUseSourceMap : isEnvDevelopment,
                 },
                 'sass-loader'
               ),
@@ -562,8 +551,7 @@ module.exports = function (webpackEnv) {
               use: getStyleLoaders({
                   importLoaders: 3,
                   sourceMap: isEnvProduction ?
-                    shouldUseSourceMap :
-                    isEnvDevelopment,
+                    shouldUseSourceMap : isEnvDevelopment,
                   modules: {
                     getLocalIdent: getCSSModuleLocalIdent,
                   },
@@ -579,8 +567,7 @@ module.exports = function (webpackEnv) {
                   importLoaders: 3,
                   modules: true,
                   sourceMap: isEnvProduction ?
-                    shouldUseSourceMap :
-                    isEnvDevelopment,
+                    shouldUseSourceMap : isEnvDevelopment,
                 },
                 'less-loader'
               ),
@@ -597,8 +584,7 @@ module.exports = function (webpackEnv) {
               use: getStyleLoaders({
                   importLoaders: 3,
                   sourceMap: isEnvProduction ?
-                    shouldUseSourceMap :
-                    isEnvDevelopment,
+                    shouldUseSourceMap : isEnvDevelopment,
                   modules: {
                     getLocalIdent: getCSSModuleLocalIdent,
                   },
@@ -617,7 +603,7 @@ module.exports = function (webpackEnv) {
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
               // by webpacks internal loaders.
-              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/,path.resolve(__dirname, '../src/assets/svg'),],
+              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/, path.resolve(__dirname, '../src/assets/svg'), ],
               options: {
                 name: 'static/media/[name].[hash:8].[ext]',
               },
@@ -644,8 +630,7 @@ module.exports = function (webpackEnv) {
             inject: true,
             template: paths.appHtml,
           },
-          isEnvProduction ?
-          {
+          isEnvProduction ? {
             minify: {
               removeComments: true,
               collapseWhitespace: true,
@@ -769,11 +754,9 @@ module.exports = function (webpackEnv) {
         async: isEnvDevelopment,
         checkSyntacticErrors: true,
         resolveModuleNameModule: process.versions.pnp ?
-          `${__dirname}/pnpTs.js` :
-          undefined,
+          `${__dirname}/pnpTs.js` : undefined,
         resolveTypeReferenceDirectiveModule: process.versions.pnp ?
-          `${__dirname}/pnpTs.js` :
-          undefined,
+          `${__dirname}/pnpTs.js` : undefined,
         tsconfig: paths.appTsConfig,
         reportFiles: [
           // This one is specifically to match during CI tests,
